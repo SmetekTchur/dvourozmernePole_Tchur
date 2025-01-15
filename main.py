fadc9205904e5fcbf11b5bc917466e004e8f8c2e
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2025-01-15 08:34:54 +0100

    DP1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("i nate higgers")
+matrix =  [[1,5,8,6],#první řádek
+          [5,7,6,2], #druhý řádek
+          [9,7,5,9],]#třetí řádek
+#vytisknutí řádků a sloupců
+print (matrix[0])
+print (matrix[1])
+print (matrix[2])
+
+    
