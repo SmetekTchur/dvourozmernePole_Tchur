9743df25794642f151ba2abd8857ef85dced80e3
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2025-01-20 11:26:45 +0100

    add_row_update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-matrix =  [[1,5,8],#první řádek
-          [5,105,6], #druhý řádek a změna druhého čísla na číslo 105
-          [9,7,5],#třetí řádek
-          [0,8,3],]# čtvrtý řádek
+matrix =  [[1,5,8,6],#první řádek
+          [5,105,6,6], #druhý řádek a změna druhého čísla na číslo 105
+          [9,7,5,9],#třetí řádek
+          [0,8,3,8],]# čtvrtý řádek
 #vytisknutí řádků a sloupců
 print (matrix[0])
 print (matrix[1])
 print (matrix[2])
+print (matrix[3])
 
     
