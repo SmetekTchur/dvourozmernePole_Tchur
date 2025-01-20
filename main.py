da596b017d078248bc6fef9e1dec689edf614289
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2025-01-20 11:10:12 +0100

    change_number1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-matrix =  [[1,5,8,6],#první řádek
-          [5,7,6,2], #druhý řádek
-          [9,7,5,9],]#třetí řádek
+matrix =  [[1,5,8],#první řádek
+          [5,105,6], #druhý řádek a změna druhého čísla na číslo 105
+          [9,7,5],]#třetí řádek
 #vytisknutí řádků a sloupců
 print (matrix[0])
 print (matrix[1])
